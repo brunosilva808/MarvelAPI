a16fe5f420d5ed5abf8f8cdf936a3db45468c9ae
Bruno Silva <bruno.silva@bestseller.com>
2019-08-28 16:58:25 +0100

    Initial commit

---
+++ MarvelAPI/MarvelAPI/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelAPI
+//
+//  Created by Bruno Silva on 28/08/2019.
+//  Copyright © 2019 Bruno Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
